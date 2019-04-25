f7fe3aae557cbc1a9378121c3c05559b478fef84
Doram13 <1991doram@gmail.com>
2019-04-25 15:25:53 +0200

    Routing to SeenPets and LostPets added

--- PetFinder/Controllers/HomeController.cs
+++ PetFinder/Controllers/HomeController.cs
@@ -4,12 +4,24 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PetFinder.Core.Models;
+using PetFinder.Data;
 using PetFinder.Models;
 
 namespace PetFinder.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+
         public IActionResult Index()
         {
             return View();
@@ -18,6 +30,16 @@ namespace PetFinder.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        public async Task<IActionResult> SeenPets()
+        {
+            return View(await _context.Posts.Where(p => p.PostType == PostTypes.SEEN).ToListAsync());
+        }
+
+        public async Task<IActionResult> LostPets()
+        {
+            return View(await _context.Posts.Where(p => p.PostType == PostTypes.LOST).ToListAsync());
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
