41cfab1c72950789eb3eddc8adac4e1b8ed31cf1
gergelyEsztojka <esztojka.gergely@gmail.com>
2019-04-25 12:58:16 +0200

    C:/Program Files/Git/Posts/CreatePost url is live and binded to post model

---
+++ PetFinder/Controllers/PostsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PetFinder.Controllers
+{
+    public class PostsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CreatePost()
+        {
+            return View();
+        }
+    }
+}
