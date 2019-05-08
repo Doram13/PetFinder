dacef8c15b61145847f07198aa71960597283cc5
Andras Horinka <horinka.andras@gmail.com>
2019-05-08 11:27:28 +0200

    updated postwith tag class - Dictionary<Tag, bool> - and set all values by default to false

--- PetFinder.Core/Models/PostWithTag.cs
+++ PetFinder.Core/Models/PostWithTag.cs
@@ -1,8 +1,21 @@
-﻿namespace PetFinder.Core.Models
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace PetFinder.Core.Models
 {
     public class PostWithTag
     {
         public Post Post { get; set; }
-        public Tag Tag { get; set; }
+        public Dictionary<Tag, bool> Tags { get; set; }
+
+        public PostWithTag()
+        {
+            foreach (Tag tag in (Tag[])Enum.GetValues(typeof(Tag)))
+            {
+                Tags[tag] = false;
+            }
+        }
+
     }
 }
