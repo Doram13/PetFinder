d940aa6ef01879f318ddd494f5a179d1268ba547
gergelyEsztojka <esztojka.gergely@gmail.com>
2019-05-09 13:00:19 +0200

    InMemoryDatabase test project added and save into database is tested

---
+++ PetFinder.InMemoryTester/PostServiceTest.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using PetFinder.Core.Models;
+using PetFinder.Data;
+using PetFinder.Service;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetFinder.InMemoryTester
+{
+    public class PostServiceTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public async Task SavePostAsync_Shoul_Insert_One_Post_Into_Db()
+        {
+            var connection = new SqliteConnection("DataSource=:memory:");
+            connection.Open();
+
+            try
+            {
+                var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                    .UseSqlite(connection)
+                    .Options;
+
+                using (var context = new ApplicationDbContext(options))
+                {
+                    context.Database.EnsureCreated();
+                }
+
+                using (var context = new ApplicationDbContext(options))
+                {
+                    var service = new PostService(context);
+                    await service.SavePostAsync(new Post() { Title = "Test Post", Description = "Test Post decription", IsActive = true, PostDate = DateTime.Now });
+                }
+
+                using (var context = new ApplicationDbContext(options))
+                {
+                    Assert.That(context.Posts.Count(), Is.EqualTo(1));
+                }
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+    }
+}
