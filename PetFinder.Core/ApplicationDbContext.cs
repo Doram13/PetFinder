63dd89a4f627954987932d7284793f3370b01d4a
Andras Horinka <horinka.andras@gmail.com>
2019-05-09 15:40:48 +0200

    bugfix

--- PetFinder.Core/ApplicationDbContext.cs
+++ PetFinder.Core/ApplicationDbContext.cs
@@ -19,6 +19,8 @@ namespace PetFinder.Data
         {
         }
 
+        public ApplicationDbContext() { }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
