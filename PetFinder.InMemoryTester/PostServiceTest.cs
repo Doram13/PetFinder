c9082c6130e83034c6ebbd35ea653e67a18355e2
gergelyEsztojka <esztojka.gergely@gmail.com>
2019-05-09 13:20:13 +0200

    Test model refactor, connection and options are class private reachable from every test

--- PetFinder.InMemoryTester/PostServiceTest.cs
+++ PetFinder.InMemoryTester/PostServiceTest.cs
@@ -12,42 +12,52 @@ namespace PetFinder.InMemoryTester
 {
     public class PostServiceTest
     {
+        private SqliteConnection Connection;
+        private DbContextOptions<ApplicationDbContext> Options;
+
         [SetUp]
-        public void Setup()
+        public async Task Setup()
         {
+            Connection = new SqliteConnection("DataSource=:memory:");
+            Connection.Open();
+
+            Options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseSqlite(Connection)
+                .Options;
+
+            using (var context = new ApplicationDbContext(Options))
+            {
+                context.Database.EnsureCreated();
+            }
+
+            using (var context = new ApplicationDbContext(Options))
+            {
+                var service = new PostService(context);
+                await service.SavePostAsync(new Post() { Title = "First Test Post", Description = "First Test Post decription", IsActive = true, PostDate = DateTime.Now });
+                await service.SavePostAsync(new Post() { Title = "Second Test Post", Description = "Second Test Post decription", IsActive = true, PostDate = DateTime.Now });
+                await service.SavePostAsync(new Post() { Title = "Third Test Post", Description = "Third Test Post decription", IsActive = true, PostDate = DateTime.Now });
+                await service.SavePostAsync(new Post() { Title = "Fourth Test Post", Description = "Fourth Test Post decription", IsActive = true, PostDate = DateTime.Now });
+            }
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Connection.Close();
         }
 
         [Test]
         public async Task SavePostAsync_Shoul_Insert_One_Post_Into_Db()
         {
-            var connection = new SqliteConnection("DataSource=:memory:");
-            connection.Open();
-
-            try
+            using (var context = new ApplicationDbContext(Options))
             {
-                var options = new DbContextOptionsBuilder<ApplicationDbContext>()
-                    .UseSqlite(connection)
-                    .Options;
-
-                using (var context = new ApplicationDbContext(options))
-                {
-                    context.Database.EnsureCreated();
-                }
-
-                using (var context = new ApplicationDbContext(options))
-                {
-                    var service = new PostService(context);
-                    await service.SavePostAsync(new Post() { Title = "Test Post", Description = "Test Post decription", IsActive = true, PostDate = DateTime.Now });
-                }
-
-                using (var context = new ApplicationDbContext(options))
-                {
-                    Assert.That(context.Posts.Count(), Is.EqualTo(1));
-                }
+                var service = new PostService(context);
+                await service.SavePostAsync(new Post() { Title = "SavePostAsync Test post", Description = "Decription", IsActive = true, PostDate = DateTime.Now });
             }
-            finally
+
+            using (var context = new ApplicationDbContext(Options))
             {
-                connection.Close();
+                Assert.That(context.Posts.Count(), Is.EqualTo(5));
             }
         }
     }
